ac10320553160052e3a31321604cbee00b30aeae
Yoon Hye Jin <lold2424@gmail.com>
2024-01-21 16:51:04 +0900

    메인 화면

---
+++ src/main/java/com/example/myworkbook/MyWorkbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.myworkbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorkbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWorkbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myworkbook/MyWorkbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myworkbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWorkbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
